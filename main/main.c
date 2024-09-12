6836bb8bea701d3063a2ce6ccbd23af023a8c6e4
Nitiphum <65030130@kmitl.ac.th>
2024-09-12 14:37:29 +0700

    commit

--- main/main.c
+++ main/main.c
@@ -7,18 +7,23 @@
 
 TaskHandle_t MyFirstTaskHandle = NULL;
 
-void My_First_Task(void* arg)
+void My_First_Task(void * arg)
 {
-    uint16_t i = 0;
-    while(1)
-    {
-        printf("Hello My Frist Task %d\n", i);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-        i++;
-    }
+	uint16_t i = 0;
+	while(1)
+	{
+		printf("Hello My First Task %d\n",i);
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+		i++;
+	}
 }
 
 void app_main(void)
 {
-    xTaskCreate(My_First_Task, "Frist_Task", 4096 , NULL ,10 ,&MyFirstTaskHandle);
+	xTaskCreate(My_First_Task, "Fitst_Task", 4096, NULL, 10, &MyFirstTaskHandle);
+	xTaskCreate(My_First_Task, "Fitst_Task", 4096, NULL, 10, &MyFirstTaskHandle);
+	xTaskCreate(My_First_Task, "Fitst_Task", 4096, NULL, 10, &MyFirstTaskHandle);
+	xTaskCreate(My_First_Task, "Fitst_Task", 4096, NULL, 10, &MyFirstTaskHandle);
+	xTaskCreate(My_First_Task, "Fitst_Task", 4096, NULL, 10, &MyFirstTaskHandle);
+
 }
